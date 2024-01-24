586b1b54b5b7b2da762617e634db8e400bbfaa55
david <davidsanchezdiaz72@gmail.com>
2024-01-24 22:02:20 +0100

    Pagina de inicio creada

---
+++ views/index.view.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&family=Slabo+27px&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Galeria</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Galeria de imagenes</h1>
+    </div>
+    <section class="fotos">
+        <div class="contenedor">
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/1.jpg">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/2.jpg">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/3.jpg">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/4.jpg">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/5.jpg">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/6.jpg">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/7.jpg">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/8.jpg">
+                </a>
+            </div>
+
+            <div class="paginacion">
+                <a href="#" class="izquierda"><i class="fa fa-long-arrow-lef"></i> Pagina Anterior</a>
+                <a href="#" class="derecha"><i class="fa fa-long-arrow-right"></i>Pagina Siguente </a>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <p class="copyright">Lorem ipsum dolor sit amet</p>
+    </footer>
+</body>
+</html>

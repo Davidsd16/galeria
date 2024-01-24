69d6d96cddf74fb0ffbc308cb771f6926d30b89e
david <davidsanchezdiaz72@gmail.com>
2024-01-24 20:45:33 +0100

    index / estilos css

--- index.view.php
+++ index.view.php
@@ -6,6 +6,17 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="contenedor">
+        <h1 class="titulo">Galeria de imagenes</h1>
+    </div>
+    <section class="fotos">
+        <div class="contenedor">
+            <div class="thunb">
+                <a href="#">
+                    <img src="fotos/1.jpg">
+                </a>
+            </div>
+        </div>
+    </section>
 </body>
 </html>

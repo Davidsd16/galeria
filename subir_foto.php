417d2567b7c38f08eb1df4116e50d14393a17346
david <davidsanchezdiaz72@gmail.com>
2024-01-25 19:28:05 +0100

    Subir imagen a la carpeta de destino (foto)

--- subir_foto.php
+++ subir_foto.php
@@ -1,2 +1,20 @@
 <?php
+
+require 'funciones.php';
+$conexion = conexion('galeria_practica', 'root', '');
+
+if (!$conexion) {
+    // header('Location: index.php');
+    die();
+}
+
+if ($_SERVER['REQUEST_METHODE'] == 'POST' && !empty($_FILES)) {
+    $check = @getimagesize($FILES['foto']['tmp_name']);
+    if ($check !== false) {
+        $carpeta_destino = 'fotos/';
+        $archivo_subido = $carpeta_destino . $_FILES['foto']['name'];
+        move_uploaded_file($_FILES['foto']['tmp_name'], $archivo_subido);
+    }
+}
+
 require 'views/subir_foto.view.php';
